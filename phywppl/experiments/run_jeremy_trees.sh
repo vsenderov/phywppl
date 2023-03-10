59a91ac75e7ec14251f8d6315678e753bd8518ae
Viktor Senderov <vsenderov@gmail.com>
2023-03-10 17:48:33 +0200

    Squashed commit of the following:
    commit fa2553bec0dcf3a8670c1e1d35d1cee963d98a15
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:47:09 2023 +0200
    
        Simulators from priors.
    
    commit 1db245c9391b04b032b7f8679a3a1892761f6907
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:46:38 2023 +0200
    
        No survivorship bias correction version of CRBD analyitcal.
    
    commit 352a35c999961488064107a4d0bc816e566c29c4
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:39:23 2023 +0200
    
        Shared simulator code back in the library
    
    commit 2bebda27d08eb890588678f5adae430c05507f15
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:38:18 2023 +0200
    
        Major update to the simulators code.
    
    commit 891c862923a385b221008509d7144fc6ec7f854a
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:36:48 2023 +0200
    
        Added an inner loop to deal with MCMC replicates.
    
    commit af358ad2fb75ddfc413fb7b2329c2e7d1d875feb
    Author: Viktor Senderov <vsenderov@gmail.com>
    Date:   Fri Mar 10 17:35:42 2023 +0200
    
        Changed the CRBD analytical dist to reflect the new priors, and use MCMC for smoothness.

--- phywppl/experiments/run_jeremy_trees.sh
+++ phywppl/experiments/run_jeremy_trees.sh
@@ -2,7 +2,7 @@ trees=("jeremy_crbd" "jeremy_bdd")
 rhos=(0.77 0.77)
 
 iters=1
-parts=200000
+parts=20000
 prefix="sims"
 interfix="_"
 
@@ -19,16 +19,32 @@ cd ..
 #  mv logz.txt model.txt tdbd.json $dir/
 # done
 
-# crbda logz loop
+# crbda logz loop (with survivorship)
 pwd
 for i in ${!trees[@]}; do
-  dir=${prefix}_${trees[$i]}_${rhos[$i]}_${interfix}_crbda_
-  mkdir $dir
-  echo "CRBD" $dir
-  npm run wppl examples/crbd-analytical.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts > logz.txt
-  npm run wppl examples/crbd-analytical-dist.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts
-  echo "crbda" > model.txt
-  mv logz.txt model.txt crbd.json $dir/
+  for j in {0..4}; do
+    dir=${prefix}_${trees[$i]}_${rhos[$i]}_${interfix}_repl${j}_crbda_
+    mkdir $dir
+    echo "CRBD" $dir
+    npm run wppl examples/crbd-analytical.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts > logz.txt
+    npm run wppl examples/crbd-analytical-dist.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts
+    echo "crbda" > model.txt
+    mv logz.txt model.txt crbd.json $dir/
+  done
+done
+
+# crbda logz loop (no survivorship with survivorship)
+pwd
+for i in ${!trees[@]}; do
+  for j in {0..4}; do
+    dir=${prefix}_${trees[$i]}_${rhos[$i]}_${interfix}_repl${j}_crbda_nosurv_
+    mkdir $dir
+    echo "CRBD no surv" $dir
+    npm run wppl examples/crbd-analytical-nosurv.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts > logz.txt
+    npm run wppl examples/crbd-analytical-dist-nosurv.wppl $iters ../data/${trees[$i]}.tre.phyjson ${rhos[$i]} $parts
+    echo "crbda_nosurv" > model.txt
+    mv logz.txt model.txt crbd.json $dir/
+  done
 done
 
 for i in ${!trees[@]}; do
